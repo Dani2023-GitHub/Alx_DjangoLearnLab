27351b9b642cadbb769a8d843a375a35978642e7
Daniel Getachew <dani.gechoo@gmail.com>
2024-11-11 21:54:50 +0300

    django auth

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path("relationship_app/", include('urls.py')),
+    path("", include("relationship_app.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
