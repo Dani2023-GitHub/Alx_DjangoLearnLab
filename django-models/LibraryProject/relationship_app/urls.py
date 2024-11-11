b46a03c9f550af11265b90e6f9c314caad046184
Daniel Getachew <dani.gechoo@gmail.com>
2024-11-11 22:30:45 +0300

    django auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .models import Book, Library
 from .views import list_books
-from .views import LibraryDetailView
+from .views import LibraryDetailView, SignUpView
 
 urlpatterns = [
     path("", list_books, name='book'),
     path("", LibraryDetailView.as_view(), name="library_detail"),
+    path("", SignUpView.as_view(), name='registration')
 ]
