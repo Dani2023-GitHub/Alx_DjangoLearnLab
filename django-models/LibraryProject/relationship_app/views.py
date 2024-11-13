e5911ffb7f81462e95ccc8de927446d337488d19
Daniel Getachew <dani.gechoo@gmail.com>
2024-11-13 22:43:03 +0300

    user Authentication added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,7 +22,7 @@ class LibraryDetailView(DetailView):
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
-    template_name = "registration/registration.html"
+    template_name = "registration/register.html"
 
 def register(request):
     if request.method == 'POST':
@@ -30,7 +30,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)  # Log in the user after successful registration
-            return redirect('home')  # Redirect to home or any page you specify
+            return redirect("home")  # Redirect to home or any page you specify
     else:
         form = UserCreationForm()
-    return render(request, 'registration/registration.html', {'form': form})
+    return render(request, 'registration/register.html', {'form': form})
