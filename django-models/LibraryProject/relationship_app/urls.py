83c897c73567ed98ac30a254f303acd229e69671
Daniel Getachew <dani.gechoo@gmail.com>
2024-11-13 22:38:51 +0300

    user Authentication added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .models import Book, Library
-from .views import list_books, register
+from . import views
 from .views import LibraryDetailView, SignUpView
 from django.contrib.auth import views
 
 urlpatterns = [
-    path("", list_books, name='book'),
+    path("", views.list_books, name='book'),
     path("", LibraryDetailView.as_view(), name="library_detail"),
     path("", SignUpView.as_view(), name='registration'),
     path("login/", views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", views.LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
-    path('register/',register, name="register"),
+    path('register/',views.register, name="register"),
 ]
