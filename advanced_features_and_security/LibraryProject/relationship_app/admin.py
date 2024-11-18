598e93d6800824350e7226159d507d00ecaf3e9f
Daniel Getachew <dani.gechoo@gmail.com>
2024-11-18 22:17:06 +0300

    advanced features and security added

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     fieldsets = UserAdmin.fieldsets + (
-        ('custom fields', {'fields': ('date_of_birth', 'profile_photo')}),
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
