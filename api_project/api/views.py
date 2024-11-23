bdec8efb85dbae4c9c458cc4ff886c59f40c9861
Daniel Getachew <dani.gechoo@gmail.com>
2024-11-24 00:29:38 +0300

    crud operation Api

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
-
+from rest_framework import viewsets
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_classes = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_classes = BookSerializer
