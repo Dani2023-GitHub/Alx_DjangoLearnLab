9d01c913a78671ebc1aabfd6f132d0041eab66e8
Daniel Getachew <dani.gechoo@gmail.com>
2024-11-24 12:12:24 +0300

    Authenticaton and permission included

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,11 +3,21 @@ from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookList(generics.ListAPIView):
+
+    #only authenticated user can view the list
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_classes = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
+
+    #Admin user can perform CRUD operation
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]    
     queryset = Book.objects.all()
     serializer_classes = BookSerializer
