793feaa478ecea03d9bc9ab45ded2190b22cb406
Daniel Getachew <dani.gechoo@gmail.com>
2024-12-02 19:20:19 +0300

    CRUD operation addded

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,25 +1,31 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-
-class ListBookView(generics.ListAPIView):
+#all users can view list but can't modify it
+class ListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+#class for authenticated user to create book
+class CreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class CreateBookView(generics.CreateAPIView):
+class DetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class DetailBookView(generics.RetrieveAPIView):
+#class to update book by autherized user
+class UpdateView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-
-class UpdateBookview(generics.UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class DeleteBookCiew(generics.DestroyAPIView):
+#class for auhterized user to delete book object
+class DeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
