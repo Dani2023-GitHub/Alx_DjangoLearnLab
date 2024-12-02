40c3c3232fdc55f83e016c09040bd83954807773
Daniel Getachew <dani.gechoo@gmail.com>
2024-12-02 20:01:56 +0300

    filtering functaionality added

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,12 +1,20 @@
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+
 #all users can view list but can't modify it
+#the class provide filtering, searching, ordering function based on object fields
 class ListView(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_fields = ['title', 'publicaton_year', 'auther']
+    search_fields = ['title', 'auther']
+    ordering_fields = ['title', 'publication_year']
 #class for authenticated user to create book
 class CreateView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
